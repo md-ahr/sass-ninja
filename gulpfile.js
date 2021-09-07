0d08b0bfe85a3fe479e1a9419ab9944777fe0640
Abdul Halim <ahr.web.pro@gmail.com>
2021-09-07 18:15:50 +0600

    Crerate custom css library using sass

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const { src, dest, watch, series } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+
+function buildStyles() {
+  return src('stylish/**/*.scss')
+    .pipe(sass())
+    .pipe(dest('css'))
+}
+
+function watchTask() {
+  watch(['stylish/**/*.scss'], buildStyles)
+}
+
+exports.default = series(buildStyles, watchTask)
